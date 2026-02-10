5324bffa1a955b6e4210292b49be79b8685fa7b4
patrick chua <patrickc@parlon.ph>
2026-02-11 00:38:49 +0800

    test

--- app/core/config.py
+++ app/core/config.py
@@ -11,16 +11,22 @@ class Settings(BaseSettings):
     BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
     # db
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
+    DB_USER: str | None = None
+    DB_PASSWORD: str | None = None
+    DB_HOST: str | None = None
+    DB_PORT: int | None = None
+    DB_NAME: str | None = None
     ###
     API_PREFIX: str  = "/api"
     JWT_HASHING_ALGORITHM: str
     JWT_MINUTES : int
     JWT_REFRESH_DAYS : int
+
+    @property
+    def database_url(self) -> str:
+        if self.DB_HOST == "sqlite":
+            return "sqlite:///:memory:"
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
 settings = Settings()
